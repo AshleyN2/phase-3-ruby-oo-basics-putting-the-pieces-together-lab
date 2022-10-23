f6defac0c0322ea712be30178027a174232fcb74
Ashley <ashleynjeri7@gmail.com>
2022-10-23 19:44:57 +0300

    passed tests

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,41 @@
-class Book 
-    def intialize (title)
-        @title = title 
+class Book
+attr_accessor :title, :author, :page_count, :genre
+     
+    def initialize(title)
+        @title = title
+    end 
 
+    def properties(title, author, page_count, genre)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts 'Flipping the page...wow, you read fast!'
+    end
+
+end 
+
+
+
+
+
+=begin
+TEST
+{
+
+Book
+  .new
+    gets initialized with a title
+  properties
+    has a title
+    has an author name
+    has a page count
+    has a genre
+  #turn_page
+    can turn the page
+
+}
+=end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,46 @@
-# Make your shoe class here!
 class Shoe
-    def intialize (type)
-        @type = type 
-    end 
+attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def properties(brand, color, size, material, condition)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble (condition='new')
+        @condition = condition
+        puts 'Your shoe is as good as new!'
+    end
+
+end
+
+
+
+
+=begin
+TEST
+{
+    
+    Shoe
+  .new
+    gets initialized with a brand
+  properties
+    has a brand
+    has a color
+    has a size
+    has a material
+    has a condition
+  #cobble
+    says that the shoe has been repaired
+Your shoe is as good as new!
+    makes the shoe's condition new
+ 
+}
+
+=end
